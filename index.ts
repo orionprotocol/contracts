10a35b41884199e83572987c0eff06089fe0a1c9
Aleksandr Kraiz <selfsurfer@gmail.com>
2022-05-29 19:11:34 +0400

    tsconfig.json

---
+++ index.ts
@@ -0,0 +1,18 @@
+export * from './ethers';
+export {
+    StakingRewardsWithLongTermBonus as Web3StakingRewardsWithLongTermBonus,
+    CustomStakingReward as Web3CustomStakingReward,
+    ERC20 as Web3ERC20,
+    Exchange as Web3Exchange,
+    IDOCollector as Web3IDOCollector,
+    IDODistributor as Web3IDODistributor,
+    IUniswapV2Pair as Web3IUniswapV2Pair,
+    IUniswapV2Router as Web3IUniswapV2Router,
+    LiquidityMigrator as Web3LiquidityMigrator,
+    OrionGovernance as Web3OrionGovernance,
+    OrionMigrator as Web3OrionMigrator,
+    OrionPoolV2Factory as Web3OrionPoolV2Factory,
+    OrionStakingReward as Web3OrionStakingReward,
+    OrionVoting as Web3OrionVoting,
+    PriceOracle as Web3PriceOracle,
+} from './web3';
